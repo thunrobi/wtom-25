4388533d2a73b05a92a99cd349e43f9fbf66bc24
thunrobi <thunrobi@arcada.fi>
2025-09-10 15:32:47 +0300

    Lektion 3 slut commit

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -6,7 +6,18 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    res.send("Helloo!")
+    res.send(req.body)
+})
+
+router.patch('/:id',(req, res) =>{
+    res.send({
+        id: req.params.id,
+        body: req.body 
+    } )
+}) 
+
+router.delete('/:id', (req, res)=>{
+    res.send({id: req.params.id})
 })
 
 module.exports = router
